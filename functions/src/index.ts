0ba3ba00d5cafe3f66e3a057dcd1361033dd43a8
Amit Inbar <inamit323@gmail.com>
2022-09-02 17:10:36 +0300

    feat: add emulators

---
+++ functions/src/index.ts
@@ -0,0 +1,171 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { HttpsError } from 'firebase-functions/lib/providers/https';
+import { DocumentSnapshot } from 'firebase-functions/v1/firestore';
+
+admin.initializeApp();
+
+enum UserRole {
+  STUDENT = 1,
+  TUTOR = 2,
+  TEACHER = 3,
+  ADMIN = 4
+}
+
+const isUserRoleAbove = async (uid: string, requestedRole: UserRole) => {
+  const user = await admin.firestore().collection('users').doc(uid).get();
+
+  const role = user.get('role');
+
+  return user.exists && role ? role >= requestedRole : false;
+};
+
+export const createUser = functions.https.onCall(async (data, context) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'The function must be called from an App Check verified app.'
+    );
+  }
+
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'User cannot access this information'
+    );
+  }
+
+  if (!isUserRoleAbove(context.auth.uid, UserRole.ADMIN)) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'User cannot access this information'
+    );
+  }
+
+  try {
+    const { uid } = await admin.auth().createUser({
+      email: data.email,
+      password: data.password
+    });
+
+    await admin.auth().setCustomUserClaims(uid, { role: data.role });
+
+    const info = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      birthDate: data.birthDate,
+      phoneNo: data.phoneNo,
+      ...(data.grade && { grade: data.grade }),
+      ...(data.group && { group: data.group }),
+      ...(data.subjects && { subjects: data.subjects }),
+      role: data.role
+    };
+
+    await admin.firestore().collection('users').doc(uid).set(info);
+  } catch (error) {
+    switch (error.code) {
+      case 'auth/email-already-exists':
+        throw new HttpsError('already-exists', 'המייל כבר קיים במערכת');
+      default:
+        throw new HttpsError('internal', 'קרתה תקלה. פנה לתמיכה');
+    }
+  }
+});
+
+export const getAllUsers = functions.https.onCall(async (data, context) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'The function must be called from an App Check verified app.'
+    );
+  }
+
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'User cannot access this information'
+    );
+  }
+
+  if (!isUserRoleAbove(context.auth.uid, UserRole.ADMIN)) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'User cannot access this information'
+    );
+  }
+
+  const { users } = await admin.auth().listUsers();
+
+  return await Promise.all(
+    users.map(async ({ uid, email, metadata, customClaims, phoneNumber }) => {
+      const userInfo: DocumentSnapshot = await admin
+        .firestore()
+        .collection('users')
+        .doc(uid)
+        .get();
+
+      const response: any = {
+        ...userInfo.data(),
+        uid,
+        email,
+        metadata,
+        customClaims,
+        phoneNumber
+      };
+
+      if (response.group) {
+        const group = (
+          await admin.firestore().collection('groups').doc(response.group).get()
+        ).data()!;
+
+        const teacher = await admin
+          .firestore()
+          .collection('users')
+          .doc(group?.teacher)
+          .get();
+
+        group.teacher = {};
+
+        group.teacher.firstName = teacher.get('firstName');
+        group.teacher.lastName = teacher.get('lastName');
+        response.group = group;
+      }
+
+      return response;
+    })
+  );
+});
+
+export const getUsersByRole = functions.https.onCall(async (data, context) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'The function must be called from an App Check verified app.'
+    );
+  }
+
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'User cannot access this information'
+    );
+  }
+
+  if (!isUserRoleAbove(context.auth.uid, UserRole.ADMIN)) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'User cannot access this information'
+    );
+  }
+
+  const docs = await admin
+    .firestore()
+    .collection('users')
+    .where('role', '==', data.role)
+    .get();
+
+  const users: any[] = [];
+  docs.forEach((doc) => users.push({ uid: doc.id, ...doc.data() }));
+
+  return users;
+});
