4d19f8be3182d45f1bf34470f92b889f49944058
Amit Inbar <inamit323@gmail.com>
2022-09-03 15:19:42 +0300

    feat: localize mui

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "@mui/material";
-import { themeCreator } from "./base";
+import React, { useState } from 'react';
+import { ThemeProvider } from '@mui/material';
+import { themeCreator } from './base';
 
 export const ThemeContext = React.createContext(
   (themeName: string): void => {}
 );
 
 const ThemeProviderWrapper = (props: any) => {
-  const curThemeName = localStorage.getItem("appTheme") || "PureLightTheme";
+  const curThemeName = localStorage.getItem('appTheme') || 'PureLightTheme';
   const [themeName, _setThemeName] = useState(curThemeName);
   const theme = themeCreator(themeName);
   const setThemeName = (themeName: string): void => {
-    localStorage.setItem("appTheme", themeName);
+    localStorage.setItem('appTheme', themeName);
     _setThemeName(themeName);
   };
 
