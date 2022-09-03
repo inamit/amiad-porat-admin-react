0f9b24384522d416394c40917b1f18750bd81905
Amit Inbar <inamit323@gmail.com>
2022-09-03 13:12:14 +0300

    feat: check if teacher is busy

--- src/content/groups/AddGroup/index.tsx
+++ src/content/groups/AddGroup/index.tsx
@@ -9,13 +9,14 @@ import {
   Button
 } from '@mui/material';
 import GenericFormFields from 'components/GenericFormFields';
-import { createNewGroup } from 'dal/groups.dal';
+import { createNewGroup, teacherHasGroupByDateTime } from 'dal/groups.dal';
 import { getDoc } from 'firebase/firestore';
 import { FormFieldType } from 'models/fieldsConfigs';
 import Group, { groupConverter } from 'models/group';
 import React, { useEffect } from 'react';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import { getHourStringFromDate } from 'utils/dateUtils';
 import { addGroupFields } from './addGroupFields';
 import { AddGroupModel } from './addGroupModel';
 
@@ -79,26 +80,55 @@ const AddGroup = (props: AddGroupProps) => {
   const addGroup = async () => {
     try {
       setLoading(true);
+      const isTeacherBusy = await teacherHasGroupByDateTime(
+        group.teacher,
+        group.dayInWeek,
+        getHourStringFromDate(group.hour)
+      );
+
+      if (isTeacherBusy) {
+        const result = await MySwal.fire({
+          icon: 'warning',
+          title: 'שים לב!',
+          text: 'המורה שנבחר תפוס בשעה שנבחרה. האם ברצונך ליצור את השיעור?',
+          confirmButtonText: 'כן',
+          showDenyButton: true,
+          denyButtonText: 'לא, חזרה לטופס',
+          allowOutsideClick: false
+        });
+
+        if (result.isDenied) {
+          return;
+        }
+      }
       const docRef = (await createNewGroup(group)).withConverter(
         groupConverter
       );
       const doc = await getDoc(docRef);
-      setLoading(false);
       MySwal.fire({ title: 'השיעור נוסף', icon: 'success' });
 
       props.onClose(undefined, 'success', doc.data());
     } catch (error) {
-      MySwal.hideLoading();
       MySwal.fire({
         title: 'לא היה ניתן להוסיף את השיעור',
         text: error.message,
         icon: 'error'
       });
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <Dialog open={props.isOpen} onClose={props.onClose}>
+    <Dialog
+      sx={{
+        '&': {
+          zIndex: 1059
+        }
+      }}
+      open={props.isOpen}
+      onClose={props.onClose}
+    >
       <DialogTitle>יצירת שיעור חדש</DialogTitle>
       <DialogContent>
         <Box
